5ecc37953eecdfc8e6bf49d5bc3420d910816e07
Güven Karanfil <guvenkaranfil235@gmail.com>
2021-09-29 22:35:00 +0300

    Improve test name

--- TimelineFeedTests/RemoteFeedLoaderTests.swift
+++ TimelineFeedTests/RemoteFeedLoaderTests.swift
@@ -18,7 +18,7 @@ class TimelineFeedTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(client: client, url: url)
