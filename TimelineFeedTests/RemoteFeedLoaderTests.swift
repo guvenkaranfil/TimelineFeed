bc1bc4552b14faaa792af1057c939e776d15d643
Güven Karanfil <guvenkaranfil235@gmail.com>
2021-09-29 22:25:58 +0300

    `RemoteFeedLoader` does not request load upon creation

---
+++ TimelineFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,43 @@
+//
+//  TimelineFeedTests.swift
+//  TimelineFeedTests
+//
+//  Created by Guven Karanfil on 29.09.2021.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class TimelineFeedTests: XCTestCase {
+
+    func test_init_doesNotRequestData() {
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(client: client)
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    // MARK - Helpers
+    
+    class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+}
