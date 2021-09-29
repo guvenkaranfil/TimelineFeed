16fdf971fd11ffa5e61df34ba4cb6da2c7a9fd3b
Güven Karanfil <guvenkaranfil235@gmail.com>
2021-09-29 21:38:51 +0300

    Create FeedItem model

---
+++ TimelineFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  TimelineFeed
+//
+//  Created by Guven Karanfil on 29.09.2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
