b2edaaeeab6ff9b58445eaf97fa80dd6bacb4a12
Güven Karanfil <guvenkaranfil235@gmail.com>
2021-09-29 22:29:27 +0300

    `RemoteFeedLoader` request data from url

--- TimelineFeedTests/RemoteFeedLoaderTests.swift
+++ TimelineFeedTests/RemoteFeedLoaderTests.swift
@@ -9,12 +9,15 @@ import XCTest
 
 class RemoteFeedLoader {
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(client: HTTPClient, url: URL) {
         self.client = client
+        self.url = url
     }
     
     func load() {
+        client.get(from: url)
     }
 }
 
@@ -25,10 +28,21 @@ protocol HTTPClient {
 class TimelineFeedTests: XCTestCase {
 
     func test_init_doesNotRequestData() {
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(client: client, url: url)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client, url: url)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURL, url)
     }
     
     // MARK - Helpers
