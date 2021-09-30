e0443eff37ef2cd23f654a7b2675af199dcc66cf
guvenkaranfil <guvenkaranfil235@gmail.com>
2021-09-30 19:45:03 +0300

    `RemoteFeedLoader` delivers error on client error

--- TimelineFeedTests/RemoteFeedLoaderTests.swift
+++ TimelineFeedTests/RemoteFeedLoaderTests.swift
@@ -35,6 +35,17 @@ class TimelineFeedTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        client.error = NSError(domain: "Test", code: 0)
+        
+        var capturedError: RemoteFeedLoader.Error?
+        sut.load { error in capturedError = error}
+        
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
     // MARK - Helpers
     
     func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -45,8 +56,12 @@ class TimelineFeedTests: XCTestCase {
     
     class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        var error: Error?
         
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }
